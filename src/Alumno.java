968f38d24d54a891a0964c877f8eef61a40cdaeb
7alvaro7 <sprec@myuax.com>
2026-03-05 09:23:23 +0100

    feat: Devuelvo todos los parametros e indico que datos son.

--- src/Alumno.java
+++ src/Alumno.java
@@ -16,4 +16,44 @@ public class Alumno {
         this.edad = edad;
     }
 
+    /**
+     * Devuelve el id del alumno
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve el nombre del alumno
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Devuelve el título de FP del alumno
+     */
+    public String getTituloFP() {
+        return tituloFP;
+    }
+
+    /**
+     * Devuelve la edad del alumno
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Devuelve la información del alumno en forma de texto
+     */
+
+    //Devuelvo toda la información indicando que es cada cosa
+    public String toString() {
+        return "ID: " + id +
+                " Nombre: " + nombre +
+                " Titulo: " + tituloFP +
+                " Edad: " + edad;
+    }
+
 }
