3c6e09eebf6f3936bce3cd08aceb63ec9681a1e7
7alvaro7 <sprec@myuax.com>
2026-03-05 09:11:35 +0100

    feat: Añadidos los atributos privados y donde se va a almacenar la información

---
+++ src/Alumno.java
@@ -0,0 +1,19 @@
+public class Alumno {
+
+
+    private int id;
+    private String nombre;
+    private String tituloFP;
+    private int edad;
+
+
+    public Alumno(int id, String nombre, String tituloFP, int edad) {
+
+        //  Aqui se guarda los valores recibidos
+        this.id = id;
+        this.nombre = nombre;
+        this.tituloFP = tituloFP;
+        this.edad = edad;
+    }
+
+}
