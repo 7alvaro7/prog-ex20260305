1c3b9064736ec31a377492af79b4044c9eddab9f
7alvaro7 <sprec@myuax.com>
2026-03-05 10:12:09 +0100

    feat: creados y registrados los tres alumnos

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+    public static void main(String[] args) {
+        CentroFP centro = new CentroFP();
+
+        // Creo los alumnos
+        Alumno a1 = new Alumno(1, "Carlos", "DAM", 20);
+        Alumno a2 = new Alumno(2, "Lucia", "DAW", 21);
+        Alumno a3 = new Alumno(3, "Miguel", "ASIR", 22);
+
+        // Registro los alumnos
+        centro.registrarAlumno(a1);
+        centro.registrarAlumno(a2);
+        centro.registrarAlumno(a3);
+
+    }
 }
